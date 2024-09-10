5a8d9a12e5b2b2464a96fb2707eb67c48ebd5ba1
Chandramohan <halemanichandramohan@gmail.com>
2024-09-10 20:13:43 +0530

    Monthly Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,32 +1,36 @@
 public class EmpWageComputation {
-    
+
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int WAGE_PER_HOUR = 20;
+    public static final int WORKING_DAYS_PER_MONTH = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
+        int totalEmpWage = 0;
         int empHours = 0;
-        int empWage = 0;
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch (empCheck) {
-            case IS_FULL_TIME:
+            if (empCheck == IS_FULL_TIME) {
                 empHours = 8;
-                System.out.println("Employee is Present (Full-Time)");
-                break;
-            case IS_PART_TIME:
+                System.out.println("Day " + day + ": Employee is Present (Full-Time)");
+            } else if (empCheck == IS_PART_TIME) {
                 empHours = 4;
-                System.out.println("Employee is Present (Part-Time)");
-                break;
-            default:
+                System.out.println("Day " + day + ": Employee is Present (Part-Time)");
+            } else {
                 empHours = 0;
-                System.out.println("Employee is Absent");
+                System.out.println("Day " + day + ": Employee is Absent");
+            }
+
+            int empWage = empHours * WAGE_PER_HOUR;
+            totalEmpWage += empWage;
+
+            System.out.println("Day " + day + ": Daily Wage: " + empWage);
         }
 
-        empWage = empHours * WAGE_PER_HOUR;
-        System.out.println("Employee Wage: " + empWage);
+        System.out.println("Total Employee Wage for the Month: " + totalEmpWage);
     }
 }
