8459ae8d9cdc6022a098bb1c2d4a06dc702b289c
Chandramohan <halemanichandramohan@gmail.com>
2024-09-10 18:38:05 +0530

    Part time Calculation

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,16 +4,22 @@ public class EmpWageComputation {
 
         int IS_FULL_TIME = 1;
         int WAGE_PER_HOUR = 20;
-        int FULL_DAY_HOUR = 8;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        int IS_PART_TIME = 2;
+        int empHours = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
 
         if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
-            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            System.out.println("Employee Daily Wage: " + dailyWage);
+            empHours = 8;
+            System.out.println("Employee is Present (Full-Time)");
+        } else if (empCheck == IS_PART_TIME) {
+            empHours = 4;
+            System.out.println("Employee is Present (Part-Time)");
         } else {
+            empHours = 0;
             System.out.println("Employee is Absent");
-            System.out.println("Employee Daily Wage: 0");
         }
+
+        int dailyWage = WAGE_PER_HOUR * empHours;
+        System.out.println("Employee Daily Wage: " + dailyWage);
     }
 }
